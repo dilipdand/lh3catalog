010f76a8a500ea7b311513bf477c3786071f3178
Dilip Dand <ddand@dvdand.com>
2016-08-03 20:40:47 -0700

    Added updates for logos

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ has_many :tags, through: :product_tags
 has_many :product_categories
 has_many :categories, through: :product_categories
 
-has_attached_file :logo, :styles => { :small => "100x100>", :med => "200x200>", :large => "514x386>" }, :default_url => ":style/fallback-product-img.png"
+has_attached_file :logo, :styles => { :small => "100x100>", :med => "200x200>", :large => "514x386>" }, :default_url => "/images/:style/fallback-product-img.png"
 validates_attachment :logo, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }, :size => { :in => 0..3.megabytes }
 
 scope :has_saas,  -> { where(saas: true)     }
